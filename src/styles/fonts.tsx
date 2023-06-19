831a1a704f9e396ae31b99a419110ac99d465c31
Mike Decker <pookmish@gmail.com>
2023-06-19 11:51:40 -0700

    try catch for sitemap.xml

--- src/styles/fonts.tsx
+++ src/styles/fonts.tsx
@@ -1,6 +1,6 @@
-import {Source_Sans_Pro} from "next/font/google";
+import {Source_Sans_3} from "next/font/google";
 
-export const sourceSansPro = Source_Sans_Pro({
+export const sourceSansPro = Source_Sans_3({
   subsets: ['latin'],
   weight: ['400', '600', '700'],
   variable: "--font-sans",
