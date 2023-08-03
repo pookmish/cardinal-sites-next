4fcdc75b7173bc98fd6ea60e41045a82b3151f29
Mike Decker <pookmish@gmail.com>
2023-08-02 23:27:01 -0700

    added google analytics and editorially tools

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,17 +2,27 @@ import '../src/styles/index.css';
 import PageHeader from "@components/global/page-header";
 import PageFooter from "@components/global/page-footer";
 import {sourceSansPro} from "../src/styles/fonts";
+import Editori11y from "@components/tools/editorially";
+import Script from "next/script";
+import GoogleAnalytics from "@components/tools/google-analytics";
 
 export const metadata = {
   title: 'Stanford University'
 }
 
-export const revalidate = 300;
+export const revalidate = 3600;
 
 const RootLayout = ({children, modal}: { children: React.ReactNode, modal?: React.ReactNode }) => {
 
   return (
     <html lang="en" className={`${sourceSansPro.className} font-sans`}>
+    <Editori11y/>
+    {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID &&
+      <>
+        <Script async src="//siteimproveanalytics.com/js/siteanalyze_80352.js"/>
+        <GoogleAnalytics/>
+      </>
+    }
     <body>
     <nav>
       <a href="#main-content" className="skiplink">Skip to main content</a>
--- middleware.tsx
+++ middleware.tsx
@@ -1,11 +1,32 @@
 import {NextRequest, NextResponse} from "next/server";
 
-const middleware = (request: NextRequest) => {
-  if (process.env.NEXT_PUBLIC_DOMAIN && process.env.NEXT_PUBLIC_DOMAIN !== request.nextUrl.host) {
-    const pathName = request.nextUrl.pathname;
-    const search = request.nextUrl.search;
-    const protocol = request.nextUrl.protocol;
-    return NextResponse.redirect(`${protocol}//${process.env.NEXT_PUBLIC_DOMAIN}${pathName}${search}`);
+export const middleware = (request: NextRequest) => {
+  const response = NextResponse.next()
+
+  if (request.nextUrl.pathname.startsWith('/api/preview')) {
+    const midnight = new Date();
+    midnight.setHours(23);
+    midnight.setMinutes(59);
+
+    // Set the cookie to expire at midnight tonight.
+    response.cookies.set({
+      name: 'addEditoria11y',
+      value: "true",
+      path: '/',
+      maxAge: Math.round(midnight.getTime() / 1000) - Math.round(new Date().getTime() / 1000)
+    })
+    return response;
   }
+
+  if (!request.nextUrl.pathname.startsWith('/api') && request.nextUrl.searchParams.get('slug')) {
+    return NextResponse.redirect(request.nextUrl.origin + request.nextUrl.pathname);
+  }
+
+  return response;
 }
-export default middleware;
+
+export const config = {
+  matcher: [
+    '/api/preview',
+  ],
+}
