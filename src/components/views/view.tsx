5e0f63ba45e1191025eb1e43a25eaa2489ee404b
Mike Decker <pookmish@gmail.com>
2023-10-18 10:32:51 -0700

    Updated dependencies

--- src/components/views/view.tsx
+++ src/components/views/view.tsx
@@ -76,7 +76,7 @@ const View = async ({viewId, displayId, args, itemsToDisplay = -1, emptyMessage,
   }
 }
 
-const getViewItems = async <T, >(view: string, itemsToDisplay: number = -1, args: string[] = []): Promise<T[]> => {
+export const getViewItems = async <T, >(view: string, itemsToDisplay: number = -1, args: string[] = []): Promise<T[]> => {
   const drupalParams = new DrupalJsonApiParams();
 
   if (args && args.length > 0) {
