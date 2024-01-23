919b56101651db67d6cda6fa34f374d4b48d98c5
Mike Decker <pookmish@gmail.com>
2024-01-23 08:51:58 -0800

    prevent cache on revalidation route

--- app/api/revalidate/route.tsx
+++ app/api/revalidate/route.tsx
@@ -1,9 +1,13 @@
 import {NextRequest, NextResponse} from "next/server";
 import {revalidatePath, revalidateTag} from "next/cache";
+import {headers} from "next/headers";
 
 // The app router doesn't correctly revalidate the paths on Vercel. This file is meant for testing and future
 // implementation when it actually works.
 export const GET = async (request: NextRequest) => {
+  // Call the headers to prevent the route from getting cached.
+  headers();
+
   const secret = request.nextUrl.searchParams.get('secret');
   if (secret !== process.env.DRUPAL_REVALIDATE_SECRET) {
     return NextResponse.json({message: 'Invalid token'}, {status: 403});
