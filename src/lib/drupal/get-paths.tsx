7a2cc0956e0b16ce694bf4753ec72d4172d724ca
Mike Decker <pookmish@gmail.com>
2024-01-24 14:44:32 -0800

    fixup

--- src/lib/drupal/get-paths.tsx
+++ src/lib/drupal/get-paths.tsx
@@ -22,6 +22,7 @@ export const pathIsValid = async (path: string): Promise<boolean> => {
   allPaths.forEach(typePaths => {
     if (typePaths.includes(path)) isValid = true;
   })
+  console.log(`valid path ${path}`, isValid);
   return isValid;
 }
 
